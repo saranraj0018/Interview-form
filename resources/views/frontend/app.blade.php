3cd931461527199274b0b7695d9a9ff75890b047
Mathan <mathasaran8@gmail.com>
2026-05-19 11:44:40 +0530

    Frontend Ui success image Completed

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -7,8 +7,10 @@
     <title>{{ $title ?? 'Hr' }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- FAVICON -->
-
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <!-- Swiper CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
